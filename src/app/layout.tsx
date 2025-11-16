bb14ae1b82eca15bdce0102c517a7b3c6429635b
Hugh <indexhui@gmail.com>
2025-11-16 22:10:37 +0800

    Analytics: 加入 Microsoft Clarity 追蹤碼於 <head>（Next Script）

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -85,6 +85,16 @@ export default function RootLayout({
             gtag('config', '${GA_ID}');
           `}
         </Script>
+        {/* Microsoft Clarity */}
+        <Script id="ms-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "u71eltbxbv");
+          `}
+        </Script>
       </head>
       <body>
         <Provider>{children}</Provider>
